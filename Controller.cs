97f230dc66f9638aaea00d5834bd9a0f33c4194b
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2023-02-04 01:04:24 -0600

    Starting on the controlling stuff.

---
+++ Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Input;
+
+
+namespace Utility
+{
+    public class KeyboardFeature
+    {
+        public bool Activated { get; set; } = false;
+    }
+    public class KeyboardManager
+    {
+    }
+}
--- Room.cs
+++ Room.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Utility
 {
@@ -17,13 +15,9 @@ namespace Utility
         public RoomFeature Previous { get; set; } = null;
         public UpdateInterface UpdateObject { get; set; } = null;
         public DrawInterface DrawObject { get; set; } = null;
-        public bool Destroyed { get; private set; } = false;
         public RoomManager Manager { get; set; } = null;
         public Queue<string> GoToQueue { get; private set; } = new Queue<string>(); // feature -> manager.
-        public Queue<object> PreviousQueue { get; private set; } = new Queue<object>(); // feature -> manager.
-        public Queue<object> ActiveQueue { get; private set; } = new Queue<object>(); // manager -> active.
-        public void Destroy() { Destroyed = true; }
-
+        public Queue<object> ActiveQueue { get; private set; } = new Queue<object>(); // manager -> feature.
     }
     public class RoomManager : UpdateInterface, DrawInterface
     {
@@ -31,9 +25,6 @@ namespace Utility
         public List<RoomFeature> Features { get; private set; } = new List<RoomFeature>();
         public void Update(float timeElapsed)
         {
-            // Remove any destroyed collision features from the manager.
-            Features.Where((x) => x.Destroyed).ToList().ForEach((x) => Features.Remove(x));
-
             // Set the Current once a RoomFeature is added to the Feature list.
             if (Current == null && Features.Count >= 1)
             {
@@ -44,25 +35,31 @@ namespace Utility
             // Perform the following operations once a Current has been established.
             if (Current != null)
             {
-                // Go to next room based on identifier.
+                // Go to next room or previous room based on identifier.
                 if (Current.GoToQueue.Count > 0)
                 {
                     string nextIdentifier = Current.GoToQueue.Dequeue();
-                    RoomFeature nextRoom = Features.Find(x => x.Identifier == nextIdentifier);
-                    if (nextRoom == null)
-                        throw new Exception($"Current {Current} with Identifier {Current.Identifier} attempted to go to nonexistent nextRoom with Identifier {nextIdentifier}.");
-                    nextRoom.Previous = Current;
-                    Current = nextRoom;
-                    Current.ActiveQueue.Enqueue(null);
-                }
 
-                // Got to previous room.
-                else if (Current.PreviousQueue.Count > 0)
-                {
-                    Current.PreviousQueue.Dequeue();
-                    if (Current.Previous == null)
-                        throw new Exception($"Current {Current} with Identifier {Current.Identifier} does not have a Previous to go to.");
-                    Current = Current.Previous;
+                    // If the nextIdentifier is specified, then go to the specified room.
+                    if (nextIdentifier != null)
+                    {
+                        RoomFeature nextRoom = Features.Find(x => x.Identifier == nextIdentifier);
+                        if (nextRoom == null)
+                            throw new Exception($"Current {Current} with Identifier {Current.Identifier} attempted to go to nonexistent nextRoom with Identifier {nextIdentifier}.");
+                        nextRoom.Previous = Current;
+                        Current = nextRoom;
+                    } 
+
+                    // If the nextIdentifier is null, then go to previous room.
+                    else
+                    {
+                        if (Current.Previous == null)
+                            throw new Exception($"Current {Current} with Identifier {Current.Identifier} does not have a Previous to go to.");
+                        Current = Current.Previous;
+                    }
+
+                    // Let the Current know it's active.
+                    Current.ActiveQueue.Enqueue(null);
                 }
 
                 // Update the Current.
